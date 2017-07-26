fce3a6df40a69dc72c455ec0eb1458e052f71d30
qjy981010 <1114943038@qq.com>
2017-07-26 11:00:49 +0800

    multi ps

--- Makefile
+++ Makefile
@@ -2,10 +2,6 @@ count : count.o trie_tree.o blockqueue.o
 	g++ count.o trie_tree.o blockqueue.o -o count -pthread -g
 count.o : count.cpp
 	g++ -c count.cpp -g
-#mutips_count : trie_tree.o mutips_count.o
-#	g++ trie_tree.o mutips_count.o -o mutips_count -pthread -g
-#mutips_count.o : mutips_count.cpp blockqueue.o
-#	g++ -c blockqueue.o mutips_count.cpp -g
 blockqueue.o : blockqueue.h blockqueue.cpp
 	g++ -c blockqueue.h blockqueue.cpp -g
 trie_tree.o : trie_tree.h trie_tree.cpp
--- ps.mk
+++ ps.mk
@@ -1,11 +1,7 @@
-#count : trie_tree.o count.o blockqueue.o
-#	g++ trie_tree.o count.o blockqueue.o -o count -pthread -g
-#count.o : count.cpp blockqueue.o
-#	g++ -c blockqueue.o count.cpp -g
-mutips_count : trie_tree.o blockqueue.o mutips_count.o
-	g++ -o mutips_count mutips_count.o trie_tree.o blockqueue.o -pthread -g
-mutips_count.o : mutips_count.cpp
-	g++ -c mutips_count.cpp -g
+mutips_count : trie_tree.o blockqueue.o multips_count.o
+	g++ -o multips_count multips_count.o trie_tree.o blockqueue.o -pthread -g
+mutips_count.o : multips_count.cpp
+	g++ -c multips_count.cpp -g
 blockqueue.o : blockqueue.h blockqueue.cpp
 	g++ -c blockqueue.h blockqueue.cpp -g
 trie_tree.o : trie_tree.h trie_tree.cpp
